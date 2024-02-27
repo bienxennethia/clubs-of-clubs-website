7082d4acfb8a528ee6e9b004bf4b8df1713e9684
Xennethia Bien <bienxennethia@gmail.com>
2024-02-27 17:58:56 +0800

    initial Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Card.scss";
+
+// images
+import { ReactComponent as Profile } from "../../icons/profile.svg";
+
+const Card = ({name, position}) => {
+
+  return (
+    <div className="card">
+      <div className="card__details">
+        <div className="card__image">
+          <Profile className="card__logo" />
+        </div>
+        <p className="card__position">{position}</p>
+        <p className="card__name">{name}</p>
+      </div>
+     </div>
+  )
+}
+
+export default Card;
