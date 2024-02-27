4cb8e9cb7290524a8c2e6ee54ca250559b2b8bad
Xennethia Bien <bienxennethia@gmail.com>
2024-02-27 18:01:21 +0800

    update app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Clubs from './components/Clubs/Clubs';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <div className="content">
+        <div className='content__background'></div>
+        <Hero />
+        <About />
+        <Clubs/>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
