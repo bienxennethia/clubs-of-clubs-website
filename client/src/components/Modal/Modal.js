a9078f761edbe7ff039fadda006e7304f7c6d9ec
Xennethia Bien <bienxennethia@gmail.com>
2024-03-17 19:31:42 +0800

    use div

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,12 +1,12 @@
 import "./Modal.scss";
 const Modal = ({ toggleLoginModal, children }) => {
   return (
-    <section className="modal">
+    <div className="modal">
       <button onClick={toggleLoginModal} className="modal__close-btn">X</button>
       <div className="modal__container container">
         {children}
       </div>
-    </section>
+    </div>
   )
 }
 
