9f7e1f23d950e1153931ea40591a1cdf760b9c99
Xennethia Bien <bienxennethia@gmail.com>
2024-03-24 21:09:38 +0800

    fetch club type

--- server/index.js
+++ server/index.js
@@ -1,8 +1,45 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
 
-app.use('/', (req, res) => {
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+  origin: 'http://localhost:3000' // Replace 'http://example.com' with your actual frontend URL
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3001, console.log('Server running on port 3001'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'club_for_cubs_db'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+app.get('/club-types', (req, res) => {
+  const query = 'SELECT * FROM club_type_table';
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+    res.json(results);
+  });
+});
