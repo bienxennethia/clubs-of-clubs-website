4219911ef27469c55c0c2371b9c6b3cc9b390e3e
Xennethia Bien <bienxennethia@gmail.com>
2024-03-26 15:15:56 +0800

    create package.json for both folder

--- server/index.js
+++ server/index.js
@@ -3,12 +3,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: 'http://localhost:3000' // Replace 'http://example.com' with your actual frontend URL
-}));
+app.use(cors());
+
 app.use(express.json());
 
-// Import API routes
 const routes = require('./db/routes');
 app.use('/', routes);
 
